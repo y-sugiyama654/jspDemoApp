2841f62705923f7dd0af649be07857fe34ccff27
Yuta Sugiyama <y.sugiyama54@gmail.com>
2021-03-29 23:03:14 +0900

    Reading HTML Form Data with Servlet

---
+++ src/com/lub2code/jsp/jspDemoApp/StudentServlet.java
@@ -0,0 +1,56 @@
+package com.lub2code.jsp.jspDemoApp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class StudentServlet
+ */
+@WebServlet("/StudentServlet")
+public class StudentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public StudentServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+
+		PrintWriter out = response.getWriter();
+
+		// Step 3: genarate HTML content.
+		out.println("<html><body>");
+
+		out.println("The Student is confirmed:"
+				+ request.getParameter("firstName") + " "
+				+ request.getParameter("lastName"));
+
+		out.println("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
