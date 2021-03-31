45ab255952b7cc362a2bd0aad0409e7ced8670ec
Yuta Sugiyama <y.sugiyama54@gmail.com>
2021-03-31 23:52:10 +0900

    StudentDataUtilクラスの実装

---
+++ src/com/lub2code/jsp/jspDemoApp/mvctwo/StudentDataUtil.java
@@ -0,0 +1,17 @@
+package com.lub2code.jsp.jspDemoApp.mvctwo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDataUtil {
+
+	public static List<Student> getStudents() {
+
+		// create empty list
+		List<Student> students = new ArrayList<>();
+
+		// add sample data
+
+		// return the list
+	}
+}
